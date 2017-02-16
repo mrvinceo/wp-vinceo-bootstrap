dc72abfc66692192345508635695b397938e4b9c
Paul Vincent <paulvincent@oca.ac.uk>
2017-02-16 16:13:16 +0000

    Changed header container class to allow full-width header.

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 <body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60" <?php body_class(); ?>>
 
 <nav class="navbar navbar-default navbar-fixed-top">
-  <div class="container">
+  <div class="header-container">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#myNavbar">
         <span class="icon-bar"></span>
